1a0ce68871419b05083f13ffb2c7905b9ea46e65
Harry <wrz29@126.com>
2016-02-24 23:57:01 +0800

    增加检查目录函数

--- updatemd5.py
+++ updatemd5.py
@@ -28,17 +28,39 @@ def md5sum(fname):
         return ""
     return m.hexdigest()
 
+def DeepDir(fname):
+    """
+    递归检查目录
+    """
+    listdir = os.listdir(fname)
+    for name in listdir:
+        if name[0] == '.':
+            continue
+        print "name", name
+        if os.path.isfile(name):
+            print name,"\t", md5sum(name)
+        elif os.path.isdir(name):
+            DeepDir(name)
+        #else:
+        #    print "it's not a file"
+
 def main():
+    """ 检查路径
+        如果未给入参，则检查当前目录
+        如果给定参数，则检查是否为目录，如果是则检验给定目录的md5，否则退出
+    """
     argvlen = len(sys.argv)
+    path = ""
     if argvlen == 1:
-        pass
+        path = os.getcwd()
     else:
         path = sys.argv[1]
         if os.path.isdir(path):
-            pass
+            path = os.path.abspath(path)
         else:
-            pass
-    print md5sum("./README.md")
+            print "it's not a path"
+            return
+    DeepDir(path)
 
 if __name__ == '__main__':
     main()
