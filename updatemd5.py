ffec8c4118d21510f337f6a559fc605258ea2d94
Harry <wrz29@126.com>
2016-02-25 14:53:22 +0800

    完善递归查找目录

--- updatemd5.py
+++ updatemd5.py
@@ -17,32 +17,39 @@ def md5sum(fname):
         else:
             fh.seek(0)
     m = hashlib.md5()
-    if isinstance(fname, basestring) and os.path.exists(fname):
-        with open(fname, "rb") as fh:
-            for chunk in read_chunks(fh):
+    try:
+        if isinstance(fname, basestring) and os.path.exists(fname):
+            with open(fname, "rb") as fh:
+                for chunk in read_chunks(fh):
+                    m.update(chunk)
+        elif fname.__class__.__name__ in ["StringIO", "StringO"] or isinstance(fname, file):
+            for chunk in read_chunks(fname):
                 m.update(chunk)
-    elif fname.__class__.__name__ in ["StringIO", "StringO"] or isinstance(fname, file):
-        for chunk in read_chunks(fname):
-            m.update(chunk)
-    else:
-        return ""
-    return m.hexdigest()
+        else:
+            return ""
+        return m.hexdigest()
+    except:
+        pass
 
 def DeepDir(fname):
     """
     递归检查目录
     """
+    if not os.path.isdir(fname):
+        print "it's not a path"
+        return
+    fname = os.path.abspath(fname)
     listdir = os.listdir(fname)
     for name in listdir:
         if name[0] == '.':
             continue
-        print "name", name
-        if os.path.isfile(name):
-            print name,"\t", md5sum(name)
-        elif os.path.isdir(name):
-            DeepDir(name)
-        #else:
-        #    print "it's not a file"
+        path = os.path.join(fname, name)
+        if os.path.isfile(path):
+            print path,"\t", md5sum(path)
+        elif os.path.isdir(path):
+            DeepDir(path)
+        else:
+            print "it's not a file or a dir"
 
 def main():
     """ 检查路径
@@ -55,11 +62,6 @@ def main():
         path = os.getcwd()
     else:
         path = sys.argv[1]
-        if os.path.isdir(path):
-            path = os.path.abspath(path)
-        else:
-            print "it's not a path"
-            return
     DeepDir(path)
 
 if __name__ == '__main__':
